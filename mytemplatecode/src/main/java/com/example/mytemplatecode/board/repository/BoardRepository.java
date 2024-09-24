06d04601aa3e44e98dcbeda5857347fc88a73f4a
devkev00 <kimminsoo116@gmail.com>
2024-09-24 18:11:03 +0900

    feat: 레포지토리 추상화

--- mytemplatecode/src/main/java/com/example/mytemplatecode/board/repository/BoardRepository.java
+++ mytemplatecode/src/main/java/com/example/mytemplatecode/board/repository/BoardRepository.java
@@ -1,46 +1,23 @@
 package com.example.mytemplatecode.board.repository;
 
 import com.example.mytemplatecode.board.entity.Board;
-import org.springframework.stereotype.Repository;
-import org.springframework.util.Assert;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
 
-@Repository
-public class BoardRepository {
-    private final AtomicLong boardIdxGenerator = new AtomicLong(0);
-    private final HashMap<Long, Board> boardMap = new HashMap<>();
-
+public interface BoardRepository {
     // 게시글 저장
-    public void save(Board board) {
-        Long boardId = boardIdxGenerator.incrementAndGet();
-        board.setId(boardId);
-        boardMap.put(boardId, board);
-    }
+    void save(Board board);
 
     // 게시글 수정
-    // 기존 save 메서드에 합쳐져 있던 것 분리
-    public void update(Board board) {
-        boardMap.replace(board.getId(), board);
-    }
+    void update(Board board);
 
     // 게시글 단건 조회
-    public Optional<Board> findById(Long id) {
-        Assert.notNull(id, "ID must not be null!");
-        return Optional.ofNullable(boardMap.get(id));
-    }
+    Optional<Board> findById(Long id);
 
     // 게시글 전체 조회
-    public List<Board> findAll() {
-        return boardMap.values().stream().toList();
-    }
+    List<Board> findAll();
 
     // 게시글 삭제
-    public void deleteById(Long id) {
-        Assert.notNull(id, "ID must not be null!");
-        boardMap.remove(id);
-    }
+    void deleteById(Long id);
 }
--- mytemplatecode/src/main/java/com/example/mytemplatecode/board/service/BoardService.java
+++ mytemplatecode/src/main/java/com/example/mytemplatecode/board/service/BoardService.java
@@ -4,51 +4,50 @@ import com.example.mytemplatecode.board.dto.request.BoardCreateRequest;
 import com.example.mytemplatecode.board.dto.request.BoardUpdateRequest;
 import com.example.mytemplatecode.board.dto.response.BoardResponse;
 import com.example.mytemplatecode.board.entity.Board;
+import com.example.mytemplatecode.board.repository.BoardMemoryRepository;
 import com.example.mytemplatecode.global.mapper.BoardMapper;
-import com.example.mytemplatecode.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class BoardService {
-    private final BoardRepository boardRepository;
+    private final BoardMemoryRepository boardMemoryRepository;
 
     // 게시글 저장
     public void save(BoardCreateRequest request) {
-        boardRepository.save(BoardMapper.from(request));
+        boardMemoryRepository.save(BoardMapper.from(request));
     }
 
     // 게시글 단건 조회
     public BoardResponse findById(Long id) {
-        Board board = boardRepository.findById(id)
+        Board board = boardMemoryRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("게시글을 찾을 수 없습니다."));
         return BoardResponse.of(board);
     }
 
     // 게시글 전체 조회
     public List<BoardResponse> findAll() {
-        List<Board> boards = boardRepository.findAll();
+        List<Board> boards = boardMemoryRepository.findAll();
         return boards.stream().map(BoardResponse::of).toList();
     }
 
     // 게시글 수정
     public BoardResponse update(BoardUpdateRequest request) {
-        Board board = boardRepository.findById(request.id())
+        Board board = boardMemoryRepository.findById(request.id())
                 .orElseThrow(() -> new RuntimeException("게시글을 찾을 수 없습니다."));
-        boardRepository.update(board.update(request));
+        boardMemoryRepository.update(board.update(request));
         return BoardResponse.of(board);
     }
 
     // 게시글 삭제
     // id에 해당하는 글이 없는 경우 예외 처리
     public void deleteById(Long id) {
-        Board board = boardRepository.findById(id)
+        Board board = boardMemoryRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("ID " + id + "에 해당하는 게시글을 찾을 수 없습니다."));
-        boardRepository.deleteById(id);
+        boardMemoryRepository.deleteById(id);
     }
 
 }
