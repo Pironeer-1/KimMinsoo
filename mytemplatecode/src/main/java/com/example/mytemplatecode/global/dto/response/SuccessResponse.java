785d7a82bd06969bf82dd0ab48f8ab4e98c89201
devkev00 <kimminsoo116@gmail.com>
2024-09-24 15:24:11 +0900

    feat: 응답 규격화

---
+++ mytemplatecode/src/main/java/com/example/mytemplatecode/global/dto/response/SuccessResponse.java
@@ -0,0 +1,33 @@
+package com.example.mytemplatecode.global.dto.response;
+
+import com.example.mytemplatecode.global.dto.result.ResponseState;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public class SuccessResponse<T> {
+    @Schema(description = "성공 여부", example = "true")
+    private boolean success = true;
+
+    @Schema(description = "상태 코드", example = "1")
+    private int code;
+
+    @Schema(description = "성공 여부", example = "성공하였습니다")
+    private String message;
+
+    @Schema(description = "응답 데이터")
+    private T result;
+
+    public SuccessResponse(int code, String message, T result) {
+        this.code = code;
+        this.message = message;
+        this.result = result;
+    }
+
+    public static <T> SuccessResponse<T> of (int code, String message, T data) {
+        return new SuccessResponse<>(code, message, data);
+    }
+
+    public static <T> SuccessResponse<T> ok (T data) {
+        return of(ResponseState.SUCCESS.getCode(), ResponseState.SUCCESS.getMessage(), data);
+    }
+
+}
